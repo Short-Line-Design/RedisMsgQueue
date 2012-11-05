a4af987885d6a219bc7fb80808521d320f537514
Dan Prescott <danpres14@gmail.com>
2012-11-04 21:53:27 -0700

    I switched the watch to a key/value instead of a hash key/field/value and also moved to a single hget instead of the hmget with no difference in behavior - the watch is still not canceling the second query.

--- examples/example.js
+++ examples/example.js
@@ -13,27 +13,80 @@ var _ = require('underscore'),
     redisMsgQueue = require('../index');
 
 // Setup the example app, queues, and groups
-var app = 'RedisMsgQueue-Example'
-  , queues = [ { name: 'queue-1' }, { name: 'queue-2' } ]
-  , groups = [ { name: 'group-a' }, { name: 'group-b' }, { name: 'group-c' }, { name: 'group-d' } ]
+var app = 'example', unqueue = null
+  , queues = [ { name: 'first' }, { name: 'second' } ]
+  , groups = [ { name: 'alpha' }, { name: 'bravo' }, { name: 'charlie' } ]
 
-// Setup the message queues
-console.log('Setup the first message queue.');
-
-msgQueue = redisMsgQueue.createRedisMsgQueue(app, queues[0].name);
-
-// Register the message queue workers
-msgQueue.register(function(error, task, callback) {
-    console.log('Processing task - ' + util.inspect(task));
-    callback();
-}, function (error, task) {
-    console.log('Registered the worker.');
-});
-
-// Add the time series to the upstream aggregation message queue
-msgQueue.enqueue(undefined, { task: 'example', description: 'This is an example' }, function(error, reply) {
+// Setup the ungrouped message queue
+console.log('Setup the ungrouped message queue.');
+redisMsgQueue.createRedisMsgQueue(app, 'unqueue', function (error, myQueue) {
     if (error) {
-        console.warn('The example task was not queued - error ' + util.inspect(error) + '.');
+        console.warn('The example code was unable to create the ungrouped message queue - ' + util.inspect(error) + '.');
     }
-    console.warn('The example task was successfully queued.');
+    // Assign the returned queue to the example ungrouped message queue
+    unqueue = myQueue;
+
+    // Register the message queue workers
+    for (var i = 0; i < 2; i++) {
+        unqueue.register(function (myTask, callback) {
+            console.log('Worker ' + i + ' - processing task ' + util.inspect(myTask) + '.');
+            callback();
+        }, function (error, worker) {
+            if (error) {
+                console.warn('The example code was unable to register the ungrouped message queue worker ' + i +
+                             ' - ' + util.inspect(error) + '.');
+            }
+            console.log('Registered ungrouped message queue worker ' + i + '.');
+        });
+    }
+
+    // Add a few tasks to the example ungrouped message queue
+    console.log('Adding a few tasks to the ungrouped message queue.');
+    for (var i = 0; i < 1; i++) {
+        var task = { name:'un-task ' + i, number: i };
+        unqueue.enqueue(undefined, task, function (error, myTask) {
+            if (error) {
+                console.warn('The example was unable to add the task ' + util.inspect(task) +
+                             ' to the ungrouped message queue - ' + util.inspect(myTask) +
+                             ', ' + util.inspect(error) + '.');
+            }
+        });
+    }
 });
+
+// Setup the grouped message queues
+/*
+console.log('Setup each grouped message queue.');
+queues.forEach(function (queue) {
+    queue.msgQueue = redisMsgQueue.createRedisMsgQueue(app, queue.name);
+
+    // Register the message queue workers
+    for (var i = 0; i < 1; i++) {
+        queue.msgQueue.register(function (myTask, callback) {
+            console.log('Worker ' + i + ' - processing task ' + util.inspect(myTask) + '.');
+            callback();
+        }, function (error, worker) {
+            if (error) {
+                console.warn('The example code was unable to register the grouped message queue worker ' + i +
+                             ' - ' + util.inspect(error) + '.');
+            }
+            console.log('Registered ' + queue.name + ' grouped message queue worker ' + i + '.');
+        });
+    }
+
+    // Add a few tasks to the each grouped message queue
+    console.log('Adding a few tasks to the grouped message queue.');
+    groups.forEach(function (group) {
+        for (var i = 0; i < 5; i++) {
+            var task = { name:'task ' + i, number: i };
+            queue.msgQueue.enqueue(group, task, function (error, myTask) {
+                if (error) {
+                    console.warn('The example was unable to add the task ' + util.inspect(task) +
+                                 ' to the grouped message queue - ' + util.inspect(myTask) +
+                                 ', ' + util.inspect(error) + '.');
+                }
+            });
+        }
+    });
+});
+*/
